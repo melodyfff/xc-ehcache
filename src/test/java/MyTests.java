62900b51463d185ed07e21a5b98af70e17d653bb
melodyfff <307208327@qq.com>
2018-03-03 23:12:57 +0800

    [xc-ehcache] :smirk: update test model

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -1,5 +1,6 @@
 import net.sf.ehcache.Cache;
 import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.Element;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
  * @date 2018/3/2 14:12
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:spring-cache.xml"})
+@ContextConfiguration(locations = {"classpath:spring-cache-test.xml"})
 public class MyTests {
 
     @Autowired
